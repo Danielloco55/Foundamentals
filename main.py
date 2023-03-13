c447e3aa1047a69865985c0da4d61ebb8af7aae2
Daniel Gonzalez <danielloco55@gmail.com>
2023-03-12 20:06:01 -0600

    second commit

---
+++ main.py
@@ -0,0 +1,12 @@
+user_option = input("rock, paper, scissors => ")
+computer_option = "random"
+
+if user_option == computer_option:
+    print("Tie")
+else user_option == "rock":
+    elif computher_option == "scissors"
+        print("rock win to scissors")
+        print("user wins")
+    else:
+        print("paper win to rock")
+        print("computer wins")
